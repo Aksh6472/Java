e5378f5527124df64ef2f598a0823775a75bb20f
Akshh6472 <Akshh6472@gmail.com>
2026-08-02 07:43:39 +0530

    Initial commit

---
+++ easy/AOR.java
@@ -0,0 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class AOR {
+   public AOR() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 10;
+      byte var2 = 15;
+      int var3 = var1 * var2;
+      System.out.println("the area of recatangle is:" + var3);
+   }
+}
